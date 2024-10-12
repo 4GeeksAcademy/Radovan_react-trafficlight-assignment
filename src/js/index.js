8e672e59c7469196ab0844b7871a69019b26e6cb
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2024-10-12 03:36:56 +0000

    Ready trafficlight

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import TrafficLight from "./component/home.jsx";
+import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<TrafficLight/>, document.querySelector("#app"));
+ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
